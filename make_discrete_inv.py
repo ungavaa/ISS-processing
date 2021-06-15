f55229c9dd4612a979ecc6317976343c5ba30e6f
Julien-Pierre Houle <39715305+jhoule42@users.noreply.github.com>
2021-06-15 10:42:04 -0400

    Update make_discrete_inv.py

--- make_discrete_inv.py
+++ make_discrete_inv.py
@@ -2,80 +2,93 @@
 #                      Make discrete inventory from ISS images
 #
 # Auteur : Julien-Pierre Houle
-# Date :   Avril 2021
+# Date :   June 2021
 # ******************************************************************************
 
+import os
 import sys
+import numpy as np
 import pandas as pd
 import progressbar
-import pytools
+sys.path.append("/home/jhoule42/git/illumina") # path to Illumina model
 import pyproj
-sys.path.append("/home/jhoule42/git/illumina")
 import pytools as pt
 
 
-# Parameters
+# PARAMETERS
 focal_dist = 0.4   # focal distance (nm)
-obs_angle = 31
+obs_angle = 31     # observer_angle
 vband_width = 100  # nm
+inventory_name = "discrete_inventory.txt"   # filename of the discrete inventory
+
+PATH_ARRAYS = "sources/np_arrays"   # PATH to the arrays load from extract_from_tif.py
+PATH_ZONES_INVENTORY = "zones_inventory.csv"
+PATH_SPCT = "Datas/spct"   # PATH to a directory containing the different lamps spectrum
+PATH_LIGHTS = "."         # Path to the LIGHTS directory
+
+# Note: You should add the desired lop file in the LIGTHS directory
+#       if the lop value wanted is missing
 
 
 # Loading np arrays files
-files = { f: np.load(f'Intrusif/np_arrays/np_{f}.npy') for f in
-                    ['domain', 'coord', 'intensity', 'tech'] }
+files = { f: np.load(f'{PATH_ARRAYS}/np_{f}.npy') for f in
+                        ['domain', 'coord', 'intensity', 'tech'] }
 
-# Extract values from zones
-df_zones = pd.read_csv('Intrusif/zone_obst.csv')
-df_zones = df_zones.sort_values('radius', ascending=False).reset_index()
+# Read values from zones inventory
+df_zones = pd.read_csv(PATH_ZONES_INVENTORY)
+df_zones = df_zones.sort_values('radius', ascending=False).reset_index(drop=True)
 zones_p = { p: df_zones[f'{p}'].to_numpy() for p in
-                    ['lat', 'lon', 'radius', 'hobs', 'dobs'] }
+                    ['lat', 'lon', 'radius', 'do', 'ho', 'fo', 'hl'] }
 
-# Convert to meters (2949)
+
+# Convert lat, lon zones to meters (2949)
 proj = pyproj.Transformer.from_crs( 4326, 2949, always_xy=False )
 i, j = proj.transform( zones_p['lat'], zones_p['lon'] )
 zones_center = np.array((i, j)).T
 zones = np.zeros((len(files['domain'])))
-hobs =  np.zeros((len(files['domain'])))
-dobs =  np.zeros((len(files['domain'])))
-
+hobs  = np.zeros((len(files['domain'])))
+dobs  = np.zeros((len(files['domain'])))
+fobs  = np.zeros((len(files['domain'])))
+hlamp = np.zeros((len(files['domain'])))
 
 print("Create circulars zones masks..")
 for idx, center in enumerate(zones_center):
     dist_from_center = np.sqrt((files['domain'][:,0] - center[0])**2 + (files['domain'][:,1] - center[1])**2)
-    mask = dist_from_center <= zones_p['radius'][idx]*1000
+    mask = dist_from_center <= zones_p['radius'][idx]
 
     zones[mask] = idx
-    hobs[zones==idx] = zones_p['hobs'][idx]
-    dobs[zones==idx] = zones_p['dobs'][idx]
+    hobs[zones==idx] = zones_p['ho'][idx]
+    dobs[zones==idx] = zones_p['do'][idx]
+    fobs[zones==idx] = zones_p['fo'][idx]
+    hlamp[zones==idx] = zones_p['hl'][idx]
 
 
 print('Calculating pow..')
-tech_equiv = {0: [np.nan,    0,  0], # spct | hlamp | lop
-              1: ['Class1',  8, '5'],
-              2: ['Class2',  8, '5'],
-              3: ['Class3',  2, '15'],
-              4: ['Class4',  8, '2'],
-              5: ['Class5',  8, '2']}
+tech_equiv = {0: [np.nan,      0],   # spct | lop
+              1: ['ClassI',   '5'],
+              2: ['ClassII',  '5'],
+              3: ['ClassIII', '15'],
+              4: ['ClassIV',  '1'],
+              5: ['ClassV',   '1']}  # Modification 2 pour 1 (car pas de 2.lop dans Lights)
 
 # Arrays valeur pour chaque pixel
 arr_spcts= np.array([tech_equiv[v][0] for v in files['tech']])
-hlamp    = np.array([tech_equiv[v][1] for v in files['tech']])
-arr_lops = np.array([tech_equiv[v][2] for v in files['tech']])
+arr_lops = np.array([tech_equiv[v][1] for v in files['tech']])
 keys = list(zip(arr_lops, arr_spcts))
 
 
-wav, norm_spectrum = np.loadtxt("Validation_Direct/Lights/photopic.dat", skiprows=1).T
-sptc = np.loadtxt('Intrusif/Lights/scotopic.dat', skiprows=1)
-wl, refl = np.loadtxt('Intrusif/Lights/asphalt.aster').T
+wav, norm_spectrum = np.loadtxt(f"{PATH_LIGHTS}/Lights/photopic.dat", skiprows=1).T
+sptc = np.loadtxt(f"{PATH_LIGHTS}/Lights/scotopic.dat", skiprows=1)
+wl, refl = np.loadtxt(f'{PATH_LIGHTS}/Lights/asphalt.aster').T
 wl *= 1000.
 refl /= 100.
 asphalt = np.interp(wav, wl, refl)
 angles = np.arange(181, dtype=float)
 
 # Load pre-normalize spcts & lops
-list_spcts = os.listdir('Datas/spct')
-spcts = {spct[:6]: pt.load_spct(wav, norm_spectrum, f'Datas/spct/{spct}') for spct in list_spcts}
-lops = {lop: pt.load_lop(angles, f'Intrusif/Lights/{lop}_pcUPLIGHT.lop') for lop in ['2','5','15']}
+list_spcts = os.listdir(PATH_SPCT)
+spcts = {spct.split("_")[0] : pt.load_spct(wav, norm_spectrum, f'{PATH_SPCT}/{spct}') for spct in list_spcts}
+lops = {lop: pt.load_lop(angles, f'{PATH_LIGHTS}/Lights/{lop}_pcUPLIGHT.lop') for lop in ['1','5','15']}
 
 S = ((408000 * 8.4e-6) / focal_dist)**2  # Taille du pixel au sol (unit/??)
 a = np.deg2rad(angles)
@@ -97,20 +110,18 @@ for lop in lops:
 
 # Attributing POW to pixels
 POW = np.zeros(len(keys))
-bar = progressbar.ProgressBar(maxval=len(keys)).start()
 for idx in range(len(keys)):
     POW[idx] = integral[keys[idx]] * files['intensity'][idx] * 1e-5 * vband_width * 10 # factor 10 to convert nm to angstrom
-    bar.update(idx)
 
 
-
-print("\nCreating discrete inventory..")
-df = pd.DataFrame(files['coord'], columns = ['lat', 'lon'])
+print("Creating discrete inventory..")
+df = pd.DataFrame(files['coord'], columns = ['#lat', 'lon'])
 df['pow'] = POW.tolist()
 df['hobs'] = hobs.tolist()
 df['dobs'] = dobs.tolist()
-df['fobs'] = 0.7
+df['fobs'] = fobs.tolist()
 df['hlamp'] = hlamp.tolist()
 df['spct'] = arr_spcts.tolist()
 df['lop'] = arr_lops.tolist()
-df.to_csv('Intrusif/discrete_inventory.txt', index=False)
+df.to_csv(inventory_name, index=False, sep=' ')
+print('Done')
